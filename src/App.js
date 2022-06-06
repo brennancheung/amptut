bb2cbf539b01227826927af8be4100f194a5cdc6
Brennan Cheung <git@brennancheung.com>
2022-06-05 21:22:00 -0700

    graphql api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,90 @@
-import logo from './logo.svg'
-import '@aws-amplify/ui-react/styles.css'
-import { Amplify } from 'aws-amplify'
+import React, { useState, useEffect } from 'react'
+import { Amplify, API } from 'aws-amplify'
 import awsExports from './aws-exports'
 import {
-  withAuthenticator,
+  Authenticator,
   Button,
   Heading,
-  Image,
   View,
   Card
 } from '@aws-amplify/ui-react'
+import { listNotes } from './graphql/queries'
+import { createNote, deleteNote } from './graphql/mutations'
+
+import '@aws-amplify/ui-react/styles.css'
 
 Amplify.configure(awsExports)
 
-function App ({ signOut }) {
+const initialFormState = { name: '', description: '' }
+const filterId = id => obj => obj.id !== id
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [formData, setFormData] = useState(initialFormState)
+
+  const fetchNotes = async () => {
+    const data = await API.graphql({ query: listNotes })
+    setNotes(data.data.listNotes.items)
+  }
+
+  const handleCreateNote = async () => {
+    if (!formData.name || !formData.description) return
+    await API.graphql({ query: createNote, variables: { input: formData } })
+    setNotes([...notes, formData])
+    setFormData(initialFormState)
+  }
+
+  const handleDeleteNote = async ({ id }) => {
+    const newNotesArray = notes.filter(filterId(id))
+    setNotes(newNotesArray)
+    await API.graphql({ query: deleteNote, variables: { input: { id } } })
+  }
+
+  const onDeleteNote = note => handleDeleteNote(note)
+
+  const updateField = field => e =>
+    setFormData({ ...formData, [field]: e.target.value })
+
+  useEffect(() => {
+    fetchNotes()
+  }, [])
+
   return (
-    <View className='App'>
-      <Card>
-        <Image src={logo} className='App-logo' alt='logo' />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign out</Button>
-    </View>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <View className='App'>
+          <Card>
+            <Heading level={1}>Welcome {user.username}!</Heading>
+          </Card>
+          <h1>Notes</h1>
+          <input
+            onChange={updateField('name')}
+            placeholder='name'
+            value={formData.name}
+          />
+          <input
+            onChange={updateField('description')}
+            placeholder='description'
+            value={formData.description}
+          />
+          <Button onClick={handleCreateNote}>Create Note</Button>
+
+          <div style={{ marginBottom: 30 }}>
+            {notes.map(note => (
+              <div key={note.id || note.name}>
+                <h2>{note.name}</h2>
+                <p>{note.description}</p>
+                <Button onClick={onDeleteNote(note)}>Delete note</Button>
+              </div>
+            ))}
+          </div>
+          <hr />
+
+          <Button onClick={signOut}>Sign out</Button>
+        </View>
+      )}
+    </Authenticator>
   )
 }
 
-export default withAuthenticator(App)
+export default App
